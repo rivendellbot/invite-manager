9993426c66885a527edc02182601492b062496d2
Bot discord creator <80044077+rivendellbot@users.noreply.github.com>
2021-03-15 14:32:47 +0100

    Update Bonus.js

--- Commands/Bonus.js
+++ Commands/Bonus.js
@@ -7,15 +7,15 @@ const Database = require("../Helpers/Database");
  * @param {Array<String>} args 
  */
 exports.run = async (client, message, args) => {
-    if(!message.member.hasPermission("ADMINISTRATOR") && !message.member.hasPermission("MANAGE_GUILD")) return message.reply("you don't have a permission.");
+    if(!message.member.hasPermission("ADMINISTRATOR") && !message.member.hasPermission("MANAGE_GUILD")) return message.reply("Vous n'avez pas la permission pour utiliser cette commande !");
 
     var victim = message.mentions.members.size > 0 ? message.mentions.members.first().id : args.length > 0 ? args[0] : undefined;
-    if(!victim) return message.reply("you didn't mention anyone.");
+    if(!victim) return message.reply("Vous n'avez pas mentionner de membre");
     victim = message.guild.member(victim);
-    if(!victim) return message.reply("the member you mentioned must be on the server.");
+    if(!victim) return message.reply("Le membre que vous mentionnez doit être dans le serveur");
 
     var num = Number(args[1]);
-    if(isNaN(num)) return message.reply("valid number.");
+    if(isNaN(num)) return message.reply("Veuillez choisir un nombre valide");
     const db = new Database("./Servers/" + message.guild.id, "Invites");
 
     var bonus = (db.add(`invites.${victim.id}.bonus`, num) || 0), total = (db.get(`invites.${victim.id}.total`) || 0);
