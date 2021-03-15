d3a7f0193dafd001fa746254d7df7c87dc1c35ad
Bot discord creator <80044077+rivendellbot@users.noreply.github.com>
2021-03-15 14:39:44 +0100

    Update and rename Me.js to invites.js

--- Commands/invites.js
+++ Commands/invites.js
@@ -11,13 +11,13 @@ exports.run = async (client, message, args) => {
     var victim = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
     var data = db.get(`invites.${victim.id}`) || { total: 0, fake: 0, inviter: null, regular: 0, bonus: 0, leave: 0 };
     var embed = new Discord.MessageEmbed()
-    .setDescription(`You have **${(data.total || 0) + (data.bonus || 0)}** invites! (**${data.regular || 0}** regular, **${data.bonus || 0}** bonus, **${data.leave || 0}** leaves, **${data.fake || 0}** fake)`)
+    .setDescription(`Vous avez **${(data.total || 0) + (data.bonus || 0)}** invites! (**${data.regular || 0}** regular, **${data.bonus || 0}** bonus, **${data.leave || 0}** membres qui sont partit, **${data.fake || 0}** fake)`)
     .setColor("RANDOM");
     message.channel.send(embed);
 };
 
 exports.conf = {
-    commands: ["me"],
+    commands: ["invites"],
     usage: "[p]me",
     enabled: true,
     guildOnly: true
